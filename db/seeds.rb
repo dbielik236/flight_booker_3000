0e6ea524b408d8b0f3b83f3ea288e11e583b911b
David Bielik <davidbielik@gmail.com>
2022-06-19 10:03:15 -0400

    created flight and airport associations and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Airport.create(city: "Manchester", code: "MHT")
+Airport.create(city: "Los Angeles", code: "LAX")
+Airport.create(city: "St. Louis", code: "STL")
+
+Flight.create(departure_airport_id: 1, arrival_airport_id: 2, date: "2022-6-31", duration: 400)
+Flight.create(departure_airport_id: 1, arrival_airport_id: 2, date: "2022-6-31", duration: 360)
+Flight.create(departure_airport_id: 1, arrival_airport_id: 2, date: "2022-6-31", duration: 300)
