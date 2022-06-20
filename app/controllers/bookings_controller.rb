9ddfc80fc20cfbc20e2fb0834bdf59f6497af367
David Bielik <davidbielik@gmail.com>
2022-06-20 11:15:44 -0400

    created form for passenger info input

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,70 +1,28 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: %i[ show edit update destroy ]
 
-  # GET /bookings or /bookings.json
-  def index
-    @bookings = Booking.all
-  end
-
-  # GET /bookings/1 or /bookings/1.json
-  def show
-  end
-
-  # GET /bookings/new
   def new
     @booking = Booking.new
-  end
-
-  # GET /bookings/1/edit
-  def edit
-  end
-
-  # POST /bookings or /bookings.json
-  def create
-    @booking = Booking.new(booking_params)
-
-    respond_to do |format|
-      if @booking.save
-        format.html { redirect_to booking_url(@booking), notice: "Booking was successfully created." }
-        format.json { render :show, status: :created, location: @booking }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @booking.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /bookings/1 or /bookings/1.json
-  def update
-    respond_to do |format|
-      if @booking.update(booking_params)
-        format.html { redirect_to booking_url(@booking), notice: "Booking was successfully updated." }
-        format.json { render :show, status: :ok, location: @booking }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @booking.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /bookings/1 or /bookings/1.json
-  def destroy
-    @booking.destroy
-
-    respond_to do |format|
-      format.html { redirect_to bookings_url, notice: "Booking was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_booking
-      @booking = Booking.find(params[:id])
+      passenger_count = params[:passenger_count].to_i
+      passenger_count.times { @booking.passengers.build }
     end
 
-    # Only allow a list of trusted parameters through.
+    def create
+        @booking = Booking.new(booking_params)
+        if @booking.save
+            redirect_to @booking
+        else
+          render :new
+        end
+    end
+
+    def show
+        @booking = Booking.find_by(id: params[:id])
+        
+    end
+
+    private 
+
     def booking_params
-      params.require(:booking).permit(:flight_id, :passenger_id)
+        params.require(:booking).permit(:flight_id, :passenger_id, passenger_attributes: [:name, :email])
     end
 end
